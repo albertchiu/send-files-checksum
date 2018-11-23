c40d66daa2827a5adae30700e31fff02ef84d81c
albertchiu <albertchiu>
2018-11-23 10:17:40 +0800

    add diff function

--- app.js
+++ app.js
@@ -4,21 +4,20 @@ const crypto = require('crypto');
 const config = require('./config.json');
 const nodemailer = require('nodemailer');
 const zlib = require('zlib');
-const filterRegExp = /(?:)/;//new RegExp('');
-const algorithm = 'sha256';
+const algorithm = 'md5';
+const snapshotDir = './snapshot';
 
 function iterateFiles(startPath, filter, callback) {
     if (!fs.existsSync(startPath)) {
         console.log('no dir ', startPath);
         return;
     }
-
     let files = fs.readdirSync(startPath);
     for (let i = 0; i < files.length; i++) {
         let filename = path.join(startPath, files[i]);
         let stat = fs.lstatSync(filename);
         if (stat.isDirectory()) {
-            iterateFiles(filename, filter, callback); //recurse
+            iterateFiles(filename, filter, callback);
         }
         else if (filter.test(filename)) {
             callback(filename);
@@ -26,17 +25,17 @@ function iterateFiles(startPath, filter, callback) {
     }
 }
 
-function checksum(str = '', algorithm, encoding) {
+function checksum(str = '', algorithm = 'md5', encoding = 'hex') {
     return crypto
-        .createHash(algorithm || 'md5')
+        .createHash(algorithm)
         .update(str, 'utf8')
-        .digest(encoding || 'hex');
+        .digest(encoding);
 }
 
 function sendmail(smtpTransport, message) {
-    smtpTransport.sendMail(message, (error, response) => {
-        if (error) {
-            console.log(error);
+    smtpTransport.sendMail(message, (err, response) => {
+        if (err) {
+            console.log(err);
         } else {
             console.log(`Send mail:  ${message.subject}`);
         }
@@ -45,20 +44,34 @@ function sendmail(smtpTransport, message) {
 
 function getMailMessage(cfg, checkResults, compress) {
     let mailSubject = cfg.mail.subject || 'no subject';
-    let mailBody = `algorithm: ${algorithm}\r\nexecution timestamp: ${new Date().toLocaleString()}\r\n\r\n`;
+    let mailBody = `algorithm: ${algorithm}\r\nexecution timestamp: ${new Date().toLocaleString()}\r\n`;
     let attachments = [];
     for (let result of checkResults) {
         let { appName, hashResult } = result;
+        let jsonStr = JSON.stringify([...hashResult], null, 2);
         let attach;
         if (compress) {
-            attach = { filename: `${appName}.txt.gz`, content: compress(hashResult), contentType: 'application/x-gzip' }
+            attach = { filename: `${appName}.txt.gz`, content: compress(jsonStr), contentType: 'application/x-gzip' }
         } else {
-            attach = { filename: `${appName}.txt`, content: hashResult, contentType: 'text/plain' }
+            attach = { filename: `${appName}.txt`, content: jsonStr, contentType: 'text/plain' }
         }
         attachments.push(attach);
-        mailBody += `${attach.filename}: ${checksum(attach.content, algorithm)}\r\n`;
-    }
 
+        mailBody += '\r\n--------------------------------------------------------------------------------------------------------------------------\r\n';
+        mailBody += `${attach.filename}: ${checksum(attach.content, algorithm)}\r\n`;
+
+        if (result.hasOwnProperty('diffResult')) {
+            let diffResult = result.diffResult;
+            if (typeof diffResult === 'string') {
+                mailBody += `\r\n${diffResult}\r\n`;
+            } else {
+                mailBody += `\r\nFiles matched count : ${diffResult.matchedCount}\r\n`;
+                mailBody += `\r\nNew files :\r\n  ${diffResult.newFiles.join('\r\n  ')}`;
+                mailBody += `\r\nModified files :\r\n  ${diffResult.modifiedFiles.join('\r\n  ')}`;
+                mailBody += `\r\nRemoved files :\r\n  ${diffResult.removedFiles.join('\r\n  ')}`;
+            }
+        }
+    }
    return {
         from: cfg.mail.mailFrom,
         to: cfg.mail.recipients,
@@ -68,30 +81,44 @@ function getMailMessage(cfg, checkResults, compress) {
     };
 }
 
-function getMailTransporter(cfg, mailer) {
+function getMailTransporter({ host = '127.0.0.1', port = 25 }, mailer) {
     return mailer.createTransport({
-        host: cfg.host || '127.0.0.1',
-        port: cfg.port || 25,
+        host: host,
+        port: port,
         secure: false
     });
 }
 
 function startCheckAndGetResult(cfg) {
     let checkResults = [];
-    let apps = cfg.apps || [];
-
-    for (let app of apps) {
+    let { apps = [] } = cfg;
+    for (let { startPath, appName, regExp, diff } of apps) {
         try {
-            let { startPath } = app;
-            let temp = [];
-            iterateFiles(startPath, filterRegExp, filename => {
+            let hashResult = new Map();
+            iterateFiles(startPath, new RegExp(regExp), filename => {
                 let contents = fs.readFileSync(filename);
                 let relativePath = path.relative(startPath, filename);
-                temp.push(`${relativePath} -> ${checksum(contents, algorithm)}`);
+                hashResult.set(relativePath, checksum(contents, algorithm));
             });
-            let { appName } = app;
-            let hashResult = temp.join('\r\n');
-            checkResults.push({ appName, hashResult });
+            let result = { appName, hashResult };
+            if (diff) {
+                if (!fs.existsSync(snapshotDir)) {
+                    fs.mkdirSync(snapshotDir);
+                }
+                if (fs.existsSync(`${snapshotDir}/${appName}.json`)) {
+                    let jsonStr = fs.readFileSync(`${snapshotDir}/${appName}.json`, 'utf8');
+                    let lastHashResult = new Map(JSON.parse(jsonStr));
+                    result.diffResult = compareMaps(lastHashResult, hashResult);
+                } else {
+                    result.diffResult = 'Snapshot has been created';
+                }
+                fs.writeFile(`${snapshotDir}/${appName}.json`, JSON.stringify([...hashResult], null, 2), 'utf8', err => {
+                    if (err) {
+                        console.log(err);
+                    }
+                });
+            }
+            checkResults.push(result);
         }
         catch (ex) {
             console.log(ex);
@@ -114,6 +141,34 @@ function go() {
         catch (ex) {
             console.log(ex);
         }
+    }
+}
+
+function compareMaps(map1, map2) {
+    let matchedCount = 0;
+    let newFileNames = [], modifiedFileNames = [], removedFileNames = [];
+
+    for (let [fileName, hash] of map1) {
+        if (map2.has(fileName)) {
+            if (hash == map2.get(fileName)) {
+                matchedCount++;
+            } else {
+                modifiedFileNames.push(fileName);
+            }
+        } else {
+            removedFileNames.push(fileName);
+        }
+    }
+    for (let [fileName, hash] of map2) {
+        if (!map1.has(fileName)) {
+            newFileNames.push(fileName);
+        }
+    }
+    return {
+        matchedCount: matchedCount,
+        newFiles: newFileNames,
+        modifiedFiles: modifiedFileNames,
+        removedFiles: removedFileNames
     }
 }
 
